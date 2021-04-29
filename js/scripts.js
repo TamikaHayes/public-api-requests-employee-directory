d5b8248eb4ae9931254f23ededbeab6e9585dfc2
TamikaHayes <tamika.hayes@gmail.com>
2021-04-28 22:13:37 -0700

    Added generateHTML function
    --maps over array of 12 random user objects to generate HTML that displays 12 employee cards in the browser

--- js/scripts.js
+++ js/scripts.js
@@ -21,13 +21,14 @@
 const peopleUrl = 'https://randomuser.me/api/?nat=us&results=12';
 const peopleDiv = document.getElementById('gallery');
 
-//Call to fetch, passing it the url of the API we want to access
+//Call to fetch, passing it the url of the data source (Random User API)
 //fetch will return a Promise and a Response object containing status code/message
 //parse Response data to JSON using .then
 //response.json will read the response and return a Promise that resolves to JSON
-//Once the Promise is resolved, the Promise (response data) can be stored in the variable 'profiles'
-//console.log (profiles)
-//add catch() method for errors
+//Once the Promise is resolved, the Promise (response data results) can be stored in the variable 'profiles'
+//console.log (profiles) ---> an array of 12 employee objects
+//Call function to generate HTML 
+//add catch() method for errors (check status code of request)
 
 
 fetch(peopleUrl)
@@ -35,4 +36,31 @@ fetch(peopleUrl)
     .then(data => {
     let profiles = data.results;
     console.log(profiles);
+    generateHTML(profiles);
     })
+
+ 
+/**
+ * Generates HTML for each employee card by mapping over profiles array; inserts cards in gallery div
+ * @param   {array}     arr - array of employee objects
+ */
+function generateHTML(arr) {
+    arr.map(staffMember => {
+        staffMember =
+        `<div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src=${staffMember.picture.large} alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${staffMember.name.first} ${staffMember.name.last}</h3>
+            <p class="card-text">${staffMember.email}</p>
+            <p class="card-text cap">${staffMember.location.city}, ${staffMember.location.state}</p>
+        </div>
+    </div>`
+
+    gallery.insertAdjacentHTML('beforeend', staffMember)
+    })
+};
+
+
+
